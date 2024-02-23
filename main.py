a19021e5be366968aac6a9f5b79ebb68a6442b6d
sebastian <haruock55@gmail.com>
2024-02-23 11:13:49 -0500

    fuck

---
+++ main.py
@@ -0,0 +1,8 @@
+from estudiante import Estudiante  # Importa la clase Estudiante desde el mhjfyfyfgyfgy
+
+t = Estudiante("Sebastian", "Perea", "100000000", "loqsea@gmail.com", "320836256")
+print(t.obtener_nombre())
+print(t.obtener_apellido())
+print(t.obtener_cedula())
+print(t.obtener_correo())
+print(t.obtener_telefono())
